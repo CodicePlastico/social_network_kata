1f21d694c9543f6009ce5628c851d6d2623c3899
ema <emanuele@codiceplastico.com>
2020-05-20 18:53:41 +0200

    WIP: subscriber

--- lib/board.ex
+++ lib/board.ex
@@ -1,12 +1,20 @@
+
+defmodule SocialNetwork.Message do
+  defstruct [:user, :text, :timestamp]
+end
+
+
 defmodule SocialNetwork.Board do
   use GenServer
 
+  defstruct [:user, :token, :messages, :subscriptions]
+
   def start_link([user, token]) do
-    GenServer.start_link(__MODULE__, [token], name: user)
+    GenServer.start_link(__MODULE__, [user, token], name: user)
   end
 
-  def init([token]) do
-    {:ok, {token, []}}
+  def init([user, token]) do
+    {:ok, %{user: user, token: token, messages: [], subscriptions: []}}
   end
 
   def publish(user, token, message) do
@@ -17,15 +25,33 @@ defmodule SocialNetwork.Board do
     GenServer.call(user, :get_messages)
   end
 
-  def handle_cast({:publish, current_token, message}, {token, messages}) do
+  def subscribe(user, _token, dest) do
+    GenServer.cast(user, {:subscribe, dest})
+  end
+
+  def handle_cast({:publish, current_token, message}, %{user: user, token: token, messages: messages} = state) do
     if(current_token == token) do
-      {:noreply, {token, [message] ++ messages}}
+      msg = %SocialNetwork.Message{user: user, text: message, timestamp: DateTime.utc_now()}
+
+      {:noreply, %{state | messages: [msg] ++ messages}}
     else
-      {:noreply, {token, messages}}
+      {:noreply, state}
     end
   end
 
-  def handle_call(:get_messages, _from, {_, messages} = state) do
-    {:reply, {:ok, messages}, state}
+  def handle_cast({:subscribe, subscription}, %{subscriptions: subscriptions} = state) do
+    {:noreply, %{state | subscriptions: subscriptions ++ [subscription]}}
+  end
+
+  def handle_call(:get_messages, _from, %{messages: messages, subscriptions: subscriptions} = state) do
+
+    other_messages = Enum.flat_map(subscriptions, fn s ->
+      {:ok, msg} = SocialNetwork.Board.get_messages(s)
+      msg
+    end)
+
+    sorted_message = Enum.sort(other_messages, fn a, b -> a.timestamp > b.timestamp end)
+
+    {:reply, {:ok, messages ++ sorted_message}, state}
   end
 end
--- test/board_test.exs
+++ test/board_test.exs
@@ -7,7 +7,7 @@ defmodule BoardTest do
 
     Board.publish(:alice, "tokenA", "hello world")
     {:ok, messages} = Board.get_messages(:alice)
-    assert messages == ["hello world"]
+    assert [%SocialNetwork.Message{user: :alice, text: "hello world", timestamp: _}] = messages
   end
 
   test "Alice post two messages, should return them in reverse order" do
@@ -18,7 +18,10 @@ defmodule BoardTest do
 
     {:ok, messages} = Board.get_messages(:alice)
 
-    assert messages == ["hello ale!", "hello world"]
+    assert [
+      %SocialNetwork.Message{user: :alice, text: "hello ale!", timestamp: _},
+      %SocialNetwork.Message{user: :alice, text: "hello world", timestamp: _}
+    ] = messages
   end
 
   test "Alice post in Bobs timeline, should fail" do
@@ -39,7 +42,26 @@ defmodule BoardTest do
     Board.publish(:alice, "tokenA", "Hello I'm alice")
     Board.publish(:bob, "tokenB", "Hello I'm bob")
 
-    assert Board.get_messages(:alice) == {:ok, ["Hello I'm alice"]}
-    assert Board.get_messages(:bob) == {:ok, ["Hello I'm bob"]}
+    assert {:ok, [%SocialNetwork.Message{user: :alice, text: "Hello I'm alice", timestamp: _}]} = Board.get_messages(:alice)
+    assert {:ok, [%SocialNetwork.Message{user: :bob, text: "Hello I'm bob", timestamp: _}]} = Board.get_messages(:bob)
   end
-end
+
+  test "Charlie can subscribe to Alice's and Bob's timelines, and view an aggregated list of all subscriptions" do
+    start_supervised!({Board, [:alice, "tokenA"]}, id: :alice)
+    start_supervised!({Board, [:bob, "tokenB"]}, id: :bob)
+    start_supervised!({Board, [:charlie, "tokenC"]}, id: :charlie)
+
+    Board.subscribe(:charlie, "tokenC", :alice)
+    Board.subscribe(:charlie, "tokenC", :bob)
+
+    Board.publish(:alice, "tokenA", "Hello I'm alice")
+    Board.publish(:bob, "tokenB", "Hello I'm bob")
+
+    {:ok, msg} = Board.get_messages(:charlie)
+
+    assert [
+      %SocialNetwork.Message{user: :bob, text: "Hello I'm bob", timestamp: _},
+      %SocialNetwork.Message{user: :alice, text: "Hello I'm alice", timestamp: _}] = msg
+  end
+
+  end
